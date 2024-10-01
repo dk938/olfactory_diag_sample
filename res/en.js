dba6a14ccc3383c3fbf5a22981e381e9224b74ed
sgmsa0011 <sgmsa0011@gmail.com>
2024-10-02 00:17:12 +0900

    fill en/ja of result detail

--- res/en.js
+++ res/en.js
@@ -43,31 +43,102 @@ const translationsEn = {
     "harmony": "Harmony",
     "detailLink": "Details",
     //balance
-    "balance-title": "Balanced type",
+    "balance-title": "Balanced Type",
     "balance-description": "Always calm and rational, with a sense of humor.",
-    "balance-trends": "You have the talent to take an appropriate distance with your emotions and tend to always think calmly. Even under pressure, your thought remains little affected and you know your limits. Contrasting with the calm appearance of your demeanor, your sense of humor is also attractive.\n\nOn the other hand, you may sometimes have difficulty understanding the emotions of others. Some may be aware of their weak emotional expressiveness. Consequently, you may tend to avoid social interactions and feel anxiety in communication.\n\nFurthermore, you may find it difficult to work in a team, which can make cooperation with others complicated. Sometimes, you may give a cold impression to others and feel uncomfortable in expressing affection.\n\nYou tend to think about things from a unique perspective, prioritizing reason over emotions, and trying to grasp things from an original angle.",
+    "balance-trends": "You have the talent to take an appropriate distance from your emotions and tend to always think calmly. Even under pressure, your thinking remains little affected and you know your limits. In contrast to the apparent calm of your appearance, your sense of humor is also attractive.\n\nOn the other hand, you may sometimes have difficulty understanding the emotions of others. Some may be aware of their low emotional expressiveness. Therefore, you may tend to avoid social interactions and feel anxiety in communication.\n\nIn addition, you may find it difficult to work in a team, which can make cooperation with others complicated. Sometimes, you may give a cold impression to others and feel uncomfortable in expressing affection.\n\nYou tend to think about things from a unique perspective, preferring reason to emotions, and trying to grasp things from an original angle.",
     "balance-flavor": "Geranium",
-    "balance-flavor-description": "The scent of geranium has a relaxing and balancing effect...",
-    "balance-other-flavours": "Tropical basil (Ocimum basilicum)...",
+    "balance-flavor-description": "The scent of geranium has a relaxing and balancing effect on emotions. This effect is due to components such as citronellol and geraniol. Balanced type people are enthusiastic about new information and knowledge, but tend to lose their emotional balance when plans or expectations do not materialize. The scent of geranium can help restore this balance.",
+    "balance-other-flavours": `
+        - Tropical Basil (Ocimum basilicum)
+        - Rosewood (Aniba rosaeodora)
+        - Wild Carrot (Daucus carota)
+        - Clove (Syzygium aromaticum)
+        - Tarragon (Artemisia dracunculus)
+        - Sweet Fennel (Foeniculum vulgare var. dulce)
+        - Bourbon Geranium (Pelargonium x asperum)
+        - Rose Geranium (Pelargonium graveolens)
+        - Sweet Marjoram (Origanum majorana)
+        - Peppermint (Mentha x piperita)
+        - Neroli (Citrus aurantium var. amara)
+        - Patchouli (Pogostemon cablin)
+        - Palmarosa (Cymbopogon martinii)
+        - Damask Rose (Rosa damascena)
+        - Sandalwood (Santalum album)
+        - Mountain Savory (Satureja montana)
+        - Clary Sage ( Salvia sclarea)
+        - Linalool Thyme (Thymus vulgaris ct. linalool)
+        - Thymol Thyme (Thymus vulgaris ct. thymol)
+    `,
     //active
-    "active-title": "Active type",
+    "active-title": "Active Type",
     "active-description": "Always energetic and lively, with a sense of adventure.",
-    "active-trends": "You have the talent to take an appropriate distance from your emotions...",
-    "active-flavor": "Lemon",
-    "active-flavor-description": "The scent of lemon has a refreshing and energizing effect...",
-    "active-other-flavours": "Orange (Citrus sinensis)...",
+    "active-trends": "You are an emotional and passionate person.\n\nYour attitude of fully dedicating yourself to achieving your goals stimulates those around you and generates the energy needed to accomplish great things.\nYou are good at expressing your ideas and seem to enjoy exploring new concepts through communication with others. You have a strong desire to improve and progress, as well as an ability to seek out interesting things without moping around.\n\nOn the other hand, you may sometimes have unstable emotions and mood swings. You may act impulsively, without sufficiently considering the consequences.\nYou may lack planning and sometimes have inconsistent behaviors. However, this could be a manifestation of your free and spontaneous nature.",
+    "active-flavor": "Lemongrass",
+    "active-flavor-description": "The scent of lemongrass has relaxing and balancing effects on energy. Active type people have a personality that is passionate about new challenges and competition, consuming a lot of energy. The scent of lemongrass meets the needs of this type of people by balancing their energy and bringing a relaxing effect.",
+    "active-other-flavours": `
+        - Caraway (Carum carvi)
+        - Lemongrass (Cymbopogon winterianus)
+        - Lemon Eucalyptus (Eucalyptus citriodora)
+        - Peppermint Eucalyptus (Eucalyptus dives)
+        - Italian Helichrysum (Helichrysum italicum)
+        - Spike Lavender (Lavandula latifolia)
+        - Lemongrass (Cymbopogon citratus)
+        - Lemon Balm (Melissa officinalis)
+        - Peppermint (Mentha x piperita)
+        - Camphor Rosemary (Rosmarinus officinalis ct. camphor)
+        - Verbenone Rosemary (Rosmarinus officinalis ct. verbenone)
+    `,
     //feminin
-    "feminin-title": "Feminin type",
+    "feminin-title": "Feminine Type",
     "feminin-description": "Always gentle and nurturing, with a sense of compassion.",
-    "feminin-trends": "You have the talent to understand and support others...",
-    "feminin-flavor": "Rose",
-    "feminin-flavor-description": "The scent of rose has a calming and soothing effect...",
-    "feminin-other-flavours": "Jasmine (Jasminum sambac)...",
+    "feminin-trends": "You are a generous and sociable person.\nYou value human relationships and are good at resolving conflicts without leaving resentment. With a joyful and positive attitude, you strive to enjoy life.\n\nOn the other hand, you tend to make decisions based on your emotions, and when faced with complex problems, it may take you some time to solve them. In addition, you may have difficulty planning and executing plans.\n\nYou tend to appreciate people who value you and are optimistic. However, it may sometimes be difficult for you to make an objective judgment, and you may become indecisive.",
+    "feminin-flavor": "Lavender",
+    "feminin-flavor-description": "The scent of lavender is known for its relaxing effects and stabilizing properties on the mind. Feminine type people are likely to be attracted to this scent to control their positive energy and find a balance.",
+    "feminin-other-flavours": `
+        - Roman Chamomile (Anthemis nobilis)
+        - Cardamom (Elettaria cardamomum)
+        - Wintergreen (Gaultheria procumbens)
+        - Spike Lavender (Lavandula latifolia)
+        - True Lavender (Lavandula angustifolia)
+        - Lavandin (Lavandula x intermedia 'Super')
+        - Green Mandarin (Citrus reticulata)
+        - Petitgrain Bitter Orange (Citrus aurantium var. amara)
+        - Ylang Ylang (Cananga odorata)
+        `,
     //harmony
-    "harmony-title": "Harmony type",
+    "harmony-title": "Harmonious Type",
     "harmony-description": "Always diplomatic and cooperative, with a sense of unity.",
-    "harmony-trends": "You have the talent to bring people together and create harmony...",
-    "harmony-flavor": "Bergamot",
-    "harmony-flavor-description": "The scent of bergamot has a calming and uplifting effect...",
-    "harmony-other-flavours": "Ylang-ylang (Cananga odorata)..."
+    "harmony-trends": "You tend to prefer a calm and stable life. You have a kindness that prevents you from refusing the requests of others, and you can devote yourself to others without expecting anything in return. You apprehend things more intuitively than logically, and you value the process rather than the result.\n\nOn the other hand, you may sometimes hesitate in decision-making. You tend to be indecisive at times. You may have low self-confidence and underestimate your abilities. Therefore, you may tend to avoid taking on new challenges or adopt a passive attitude.\n\nIn addition, you may depend a lot on others and some may feel lacking in autonomy. You tend to become emotional easily and low stress resistance is also characteristic.\n\nYou tend to value the balance between yourself and others. You may sometimes be troubled by the search for a balance between finding your own value and maintaining harmony with others.",
+    "harmony-flavor": "Eucalyptus",
+    "harmony-flavor-description": "The scent of eucalyptus has an effect that increases stress resistance. This effect is due to a component called 1,8-cineole. Harmonious type people tend to be very caring towards others and easily tired of stress from human relationships. It can be expected that the scent of eucalyptus will increase stress resistance and awaken inner vitality.",
+    "harmony-other-flavours": `
+        - Angelica (Angelica archangelica)
+        - Atlas Cedarwood (Cedrus atlantica)
+        - German Chamomile (Matricaria recutita)
+        - Cajeput (Melaleuca cajeputi)
+        - Cedarwood (Juniperus oxycedrus)
+        - Labdanum (Cistus ladaniferus)
+        - Copaiba (Copaifera officinalis)
+        - Cypress (Cupressus sempervirens)
+        - Frankincense (Boswellia carterii)
+        - Black Spruce (Picea mariana)
+        - Eucalyptus (Eucalyptus globulus)
+        - Eucalyptus Radiata (Eucalyptus radiata)
+        - Juniper (Juniperus communis)
+        - Ginger (Zingiber officinale)
+        - Bay Laurel (Laurus nobilis)
+        - Lemongrass (Cymbopogon citratus)
+        - Mastic (Pistacia lentiscus)
+        - Myrrh (Commiphora myrrha)
+        - Myrtle (Myrtus communis)
+        - Niaouli (Melaleuca quinquenervia)
+        - Sweet Orange (Citrus sinensis)
+        - Oregano (Origanum vulgare)
+        - Grapefruit (Citrus paradisi)
+        - Scotch Pine (Pinus sylvestris)
+        - Ravintsara (Cinnamomum camphora)
+        - Rosemary (Rosmarinus officinalis ct. cineole)
+        - Tea Tree (Melaleuca alternifolia)
+        - Vetiver (Vetiveria zizanioides)
+        `
 };
--- res/ja.js
+++ res/ja.js
@@ -44,30 +44,101 @@ const translationsJa = {
     "detailLink": "詳細リンク",
     //balance
     "balance-title": "バランスタイプ",
-    "balance-description": "常に冷静で理性的、ユーモアのセンスを持っています。",
-    "balance-trends": "あなたは感情と適切な距離を取る才能があり、常に冷静に考える傾向があります。圧力の下でも、あなたの考えはほとんど影響を受けず、あなたの限界を知っています。あなたの態度の穏やかさとは対照的に、あなたのユーモアのセンスも魅力的です。\n\n一方で、他人の感情を理解するのが難しいことがあります。一部の人は自分の感情表現が弱いことに気づいているかもしれません。その結果、社会的な相互作用を避ける傾向があり、コミュニケーションで不安を感じることがあります。\n\nさらに、チームでの作業が難しいことがあり、他の人との協力が複雑になることがあります。時には、他人に冷たい印象を与え、愛情を表現することに不快感を感じることがあります。\n\nあなたは、ユニークな視点から物事を考え、理性を感情よりも優先し、物事をオリジナルな角度から捉えようとします。",
+    "balance-description": "常に冷静で合理的で、ユーモアのセンスがあります。",
+    "balance-trends": "あなたは感情と適切な距離を保つ才能があり、常に冷静に考える傾向があります。圧力の中でも、あなたの考えはあまり影響を受けず、自分の限界を知っています。外見の冷静さとは対照的に、あなたのユーモアのセンスも魅力的です。\n\n一方で、他人の感情を理解するのが難しいことがあるかもしれません。感情表現が少ないことに気づく人もいます。そのため、社交的な交流を避ける傾向があり、コミュニケーションで不安を感じることがあります。\n\nさらに、チームでの作業が難しいことがあり、他の人との協力が複雑になることがあります。時には、他の人に冷たい印象を与えることがあり、愛情表現に不安を感じることがあります。\n\nあなたは、理性を感情よりも重視し、物事を独自の視点で考え、オリジナルな視点で物事を捉えようとする傾向があります。",
     "balance-flavor": "ゼラニウム",
-    "balance-flavor-description": "ゼラニウムの香りはリラックスとバランスをもたらします...",
-    "balance-other-flavours": "トロピカルバジル (Ocimum basilicum)...",
+    "balance-flavor-description": "ゼラニウムの香りは、感情をリラックスさせ、バランスを取る効果があります。これは、シトロネロールやゲラニオールなどの成分によるものです。バランスタイプの人は、新しい情報や知識に熱心ですが、計画や期待が実現しないと感情のバランスを失いがちです。ゼラニウムの香りは、このバランスを取り戻すのに役立ちます。",
+    "balance-other-flavours": `
+        - トロピカルバジル（Ocimum basilicum）
+        - ローズウッド（Aniba rosaeodora）
+        - ワイルドキャロット（Daucus carota）
+        - クローブ（Syzygium aromaticum）
+        - タラゴン（Artemisia dracunculus）
+        - 甘いフェンネル（Foeniculum vulgare var. dulce）
+        - ブルボンゼラニウム（Pelargonium x asperum）
+        - ロザットゼラニウム（Pelargonium graveolens）
+        - マジョラム（Origanum majorana）
+        - ペパーミント（Mentha x piperita）
+        - ネロリ（Citrus aurantium var. amara）
+        - パチョリ（Pogostemon cablin）
+        - パルマローザ（Cymbopogon martinii）
+        - ダマスクローズ（Rosa damascena）
+        - サンダルウッド（Santalum album）
+        - 山サリエット（Satureja montana）
+        - クラリセージ（ Salvia sclarea）
+        - リナロールタイム（Thymus vulgaris ct. linalool）
+        - タイモールタイム（Thymus vulgaris ct. thymol）
+    `,
     //active
     "active-title": "アクティブタイプ",
-    "active-description": "常にエネルギッシュで活気に満ち、冒険心を持っています。",
-    "active-trends": "感情から適切な距離を置く才能があります...",
-    "active-flavor": "レモン",
-    "active-flavor-description": "レモンの香りはリフレッシュと活力をもたらします...",
-    "active-other-flavours": "オレンジ (Citrus sinensis)...",
+    "active-description": "常にエネルギッシュで生き生きとしており、冒険心があります。",
+    "active-trends": "あなたは感情的で情熱的な人です。\n\nあなたの目標に全力で取り組む姿勢は、周囲を刺激し、大きなことを成し遂げるために必要なエネルギーを生み出します。\nあなたは自分の考えを表現するのが上手で、他の人とのコミュニケーションを通じて新しいコンセプトを探求することが好きです。自分を向上させ、進歩させる強い欲求と、退屈せずに面白いことを探求する能力を持っています。\n\n一方で、感情が不安定で気分が浮き沈みすることがあります。衝動的に行動することがあり、結果を十分に考慮せずに行動することがあります。\n計画を立てることが苦手で、時には不一致な行動をとることがあります。しかし、これはあなたの自由で自発的な性格の表れかもしれません。",
+    "active-flavor": "レモングラス",
+    "active-flavor-description": "レモングラスの香りは、エネルギーをリラックスさせ、バランスを取る効果があります。アクティブタイプの人は、新しい挑戦や競争に燃える性格で、多くのエネルギーを消費します。レモングラスの香りは、このタイプの人のニーズに応え、エネルギーをバランスさせ、リラックスさせる効果があります。",
+    "active-other-flavours": `
+        - キャラウェイ（Carum carvi）
+        - レモングラス（Cymbopogon winterianus）
+        - ユーカリプタスシトリオドラ（Eucalyptus citriodora）
+        - ユーカリプタスダイブス（Eucalyptus dives）
+        - イタリアンヘリクリサム（Helichrysum italicum）
+        - ラベンダー（Lavandula latifolia）
+        - レモングラス（Cymbopogon citratus）
+        - メリッサ（Melissa officinalis）
+        - ペパーミント（Mentha x piperita）
+        - カンファーローズマリン（Rosmarinus officinalis ct. camphor）
+        - バーベノンローズマリン（Rosmarinus officinalis ct. verbenone）
+    `,
     //feminin
     "feminin-title": "フェミニンタイプ",
-    "feminin-description": "常に優しく、思いやりの心を持っています。",
-    "feminin-trends": "他人を理解し、サポートする才能があります...",
-    "feminin-flavor": "ローズ",
-    "feminin-flavor-description": "ローズの香りは落ち着きと安らぎをもたらします...",
-    "feminin-other-flavours": "ジャスミン (Jasminum sambac)...",
+    "feminin-description": "常に優しく、思いやりがあり、共感力があります。",
+    "feminin-trends": "あなたは寛大で社交的な人です。人間関係を大切にし、感情的な衝突を残さずに紛争を解決するのが得意です。明るく前向きな姿勢で、人生を楽しもうと努力しています。\n\n一方で、感情に基づいて判断を下すことがあり、複雑な問題に直面すると時間がかかることがあります。また、計画を立てて実行することが苦手であることがあります。\n\nあなたは、あなたを評価してくれる人を大切にし、楽観的です。しかし、客観的な判断を下すのが難しいことがあり、迷いがちです。",
+    "feminin-flavor": "ラベンダー",
+    "feminin-flavor-description": "ラベンダーの香りは、リラックス効果と精神を安定させる効果があります。フェミニンタイプの人は、この香りに惹かれて、ポジティブなエネルギーをコントロールし、バランスを取ろうとする傾向があります。",
+    "feminin-other-flavours": `
+        - ローマンカモミール（Anthemis nobilis）
+        - カルダモン（Elettaria cardamomum）
+        - ゴールサーリー（Gaultheria procumbens）
+        - ラベンダー（Lavandula latifolia）
+        - ラベンダーファイン（Lavandula angustifolia）
+        - ラベンダーグロス（Lavandula x intermedia 'Super'）
+        - グリーンマンダリン（Citrus reticulata）
+        - ペチグレンビガレ（Citrus aurantium var. amara）
+        - イランイラン（Cananga odorata）
+        `,
     //harmony
     "harmony-title": "ハーモニータイプ",
-    "harmony-description": "常に外交的で協力的、統一感を持っています。",
-    "harmony-trends": "人々を一つにまとめ、調和を生み出す才能があります...",
-    "harmony-flavor": "ベルガモット",
-    "harmony-flavor-description": "ベルガモットの香りは落ち着きと高揚感をもたらします...",
-    "harmony-other-flavours": "イランイラン (Cananga odorata)..."
+    "harmony-description": "優しく、他人に尽くすことができます。",
+    "harmony-trends": "あなたは、穏やかで安定した生活を好む傾向があります。他人の要求を断ることができない優しさを持ち、他人に尽くすことができます。直感的に物事を捉え、プロセスを重視し、結果を重視しません。\n\n一方で、時には意思決定に迷うことがあります。時には迷いがちです。自己信頼心が低く、自分の能力を過小評価することがあります。そのため、新しい挑戦を避けたり、受動的な姿勢をとることがあります。\n\nさらに、他人に依存しすぎることがあり、自立心が欠けることがあります。感情的になりやすく、ストレスに弱いことが特徴です。\n\nあなたは、自分と他人のバランスを重視する傾向があります。自分の価値を見つけることと、他人との調和を保つことのバランスを取ることに、時には悩むことがあります。",
+    "harmony-flavor": "ユーカリ",
+    "harmony-flavor-description": "ユーカリの香りは、ストレス耐性を高める効果があります。この効果は、1,8-シネオールという成分によるものです。ハーモニータイプの人は、他人に対して非常に思いやりがあり、人間関係からのストレスに疲れやすい傾向があります。ユーカリの香りは、ストレス耐性を高め、内なる活力を目覚めさせることが期待できます。",
+    "harmony-other-flavours": `
+        - アンジェリカ（Angelica archangelica）
+        - アトラスシーダーウッド（Cedrus atlantica）
+        - ジャーマンカモミール（Matricaria recutita）
+        - カジェプット（Melaleuca cajeputi）
+        - スギ（Juniperus oxycedrus）
+        - ラボウ（Cistus ladaniferus）
+        - コパイバ（Copaifera officinalis）
+        - クリプレス（Cupressus sempervirens）
+        - インセンス（Boswellia carterii）
+        - ブラックスプルース（Picea mariana）
+        - ユーカリプタスグロブルス（Eucalyptus globulus）
+        - ユーカリプタスラディアータ（Eucalyptus radiata）
+        - ジュニパー（Juniperus communis）
+        - ショウガ（Zingiber officinale）
+        - ローレル（Laurus nobilis）
+        - レモングラス（Cymbopogon citratus）
+        - マスティック（Pistacia lentiscus）
+        - ミルラ（Commiphora myrrha）
+        - マート（Myrtus communis）
+        - ニアウリ（Melaleuca quinquenervia）
+        - スイートオレンジ（Citrus sinensis）
+        - オレガノ（Origanum vulgare）
+        - グレープフルーツ（Citrus paradisi）
+        - ピン（Pinus sylvestris）
+        - ラヴィンツァラ（Cinnamomum camphora）
+        - ローズマリン（Rosmarinus officinalis ct. cineole）
+        - ティートリー（Melaleuca alternifolia）
+        - ベチバー（Vetiveria zizanioides）
+        `
 };
